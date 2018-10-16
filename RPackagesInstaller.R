19cfb42a1f717bcc02bf6b3aae9e7110f3466e18
laurelbrehm <laurelbrehm@users.noreply.github.com>
2018-10-16 12:33:35 +0200

    Add files via upload

---
+++ RPackagesInstaller.R
@@ -0,0 +1,10 @@
+## here's a list of packages to install for the 18th October Nijmegen R Ladies workshop.
+## run the lines you need to install the packages you don't have yet
+
+install.packages("rmarkdown")  #R markdown (an aptly named package)
+install.packages("ggplot2") #nice plotting commands
+install.packages("dplyr") #tidying and data tabulating
+install.packages("lme4") #mixed effect models
+install.packages("effects") #for plotting model output
+install.packages("knitr") #for pretty tables
+install.packages("kableExtra") #for REALLY pretty tables
